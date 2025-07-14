e63fafc9193ed342c9e210edaa298a71aedf162e
Sarvani28 <saisarvanichaganti@gmail.com>
2025-07-14 21:41:06 +0530

    UPDATING THE CORS PORT

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express()
 // to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+app.use(cors())
 
 // import routes
 import authRouter from "./routes/auth.route.js"
